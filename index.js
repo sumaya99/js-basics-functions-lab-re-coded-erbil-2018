24c4e1138dc2cfda4976a3b1cda6a422dec33059
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 03:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+function distanceFromHqInFeet(blockNumber){
+  
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(blockNumber){
+  
+  
+}
